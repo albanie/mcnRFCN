8ce2104b54bc182e9d9328c85a87f015c3756691
albanie <albanie@robots.ox.ac.uk>
2017-09-25 11:21:58 +0100

    add deps

--- setup_mcnRFCN.m
+++ setup_mcnRFCN.m
@@ -2,8 +2,46 @@ function setup_mcnRFCN()
 %SETUP_MCNRFCN Sets up mcnRFCN, by adding its folders 
 % to the Matlab path, as well as setting up mcnFasterRCNN as a dependency
 
+  % add dependencies
+  check_dependency('mcnFasterRCNN') ;
+
   root = fileparts(mfilename('fullpath')) ;
-  addpath(root) ;
   addpath(root, [root '/matlab'], [root '/pascal'], [root '/core']) ;
-  addpath( [root '/matlab/mex'], [root '/misc'], [root '/coco']) ;
+  addpath([root '/matlab/mex'], [root '/misc'], [root '/coco']) ;
   run(fullfile(vl_rootnn, 'contrib/mcnFasterRCNN/setup_mcnFasterRCNN')) ;
+
+
+% -----------------------------------
+function check_dependency(moduleName)
+% -----------------------------------
+
+  name2path = @(name) strrep(name, '-', '_') ;
+  setupFunc = ['setup_', name2path(moduleName)] ;
+  if exist(setupFunc, 'file')
+    vl_contrib('setup', moduleName) ;
+  else
+    % try adding the module to the path
+    addpath(fullfile(vl_rootnn, 'contrib', moduleName)) ;
+    if exist(setupFunc, 'file')
+      vl_contrib('setup', moduleName) ;
+    else
+      waiting = true ;
+      msg = ['module %s was not found on the MATLAB path. Would you like ' ...
+             'to install it now? (y/n)\n'] ;
+      prompt = sprintf(msg, moduleName) ;
+      while waiting
+        str = input(prompt,'s') ;
+        switch str
+          case 'y'
+            vl_contrib('install', moduleName) ;
+            vl_contrib('compile', moduleName) ;
+            vl_contrib('setup', moduleName) ;
+            return ;
+          case 'n'
+            throw(exception) ;
+          otherwise
+            fprintf('input %s not recognised, please use `y` or `n`\n', str) ;
+        end
+      end
+    end
+  end
